d8aca06d125363a1272b7a6835e8d7b9247507e1
Miguel <bydemon101@gmail.com>
2024-11-26 13:25:26 -0500

    2.5

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Importa Link
 
 const Dashboard = () => {
   const [videos, setVideos] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(''); // Estado para la barra de búsqueda
-  const [filteredVideos, setFilteredVideos] = useState([]); // Videos filtrados
 
   useEffect(() => {
     const fetchVideos = async () => {
       try {
         const response = await axios.get('https://videohub-backed.vercel.app/videos');
+        //const response = await axios.get('http://localhost:5000/videos'); // Asegúrate de que la URL sea correcta
         setVideos(response.data);
-        setFilteredVideos(response.data); // Inicializar videos filtrados
       } catch (error) {
         console.error("Error al obtener los videos:", error);
       }
@@ -21,46 +19,20 @@ const Dashboard = () => {
     fetchVideos();
   }, []);
 
-  // Filtrar los videos en función del término de búsqueda
-  const handleSearch = (e) => {
-    const term = e.target.value.toLowerCase();
-    setSearchTerm(term);
-    const filtered = videos.filter((video) =>
-      video.nombre.toLowerCase().includes(term) // Filtra por el nombre del video
-    );
-    setFilteredVideos(filtered);
-  };
-
   return (
     <div>
       <h1>Videos Subidos</h1>
-
-      {/* Barra de búsqueda */}
-      <input
-        type="text"
-        placeholder="Buscar por nombre de video..."
-        value={searchTerm}
-        onChange={handleSearch}
-        style={{
-          padding: '10px',
-          width: '300px',
-          marginBottom: '20px',
-          fontSize: '16px',
-        }}
-      />
-
       <ul>
-        {filteredVideos.map((video, index) => (
+        {videos.map((videoName, index) => (
           <li key={index}>
             <video width="320" height="240" controls>
-              <source src={video.url} type="video/mp4" />
+              <source src={`https://videohubmiguel.s3.us-east-2.amazonaws.com/${videoName}`} type="video/mp4" />
               Tu navegador no soporta el elemento de video.
             </video>
-            <p>{video.nombre}</p>
+            <p>{videoName}</p>
           </li>
         ))}
       </ul>
-
       {/* Botón para ir al perfil */}
       <Link to="/profile">
         <button style={{ marginTop: '20px', padding: '10px 20px', fontSize: '16px' }}>
